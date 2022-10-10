15a1401518669ecc38054bf3a2a49042966680e3
leosavegard <carl-leo-benjamin@hotmail.com>
2022-10-10 10:18:57 +0200

    föra över till annan dator

--- recipe-project-group4/src/views/HomepageView.vue
+++ recipe-project-group4/src/views/HomepageView.vue
@@ -2,14 +2,20 @@
     <h1>This is Homepage</h1>
 
     <recipe-item></recipe-item>
+    <categori-nav-bar></categori-nav-bar>
+   
+    
 
 </template>
 
 <script>
     import RecipeItem from '../components/recipeItems.vue'
+    import CategoriNavBar from '../components/categoriesNav.vue'
 export default {
     components: {
-        RecipeItem
+        RecipeItem,
+        CategoriNavBar
+
     }   
 }
 </script>
